77113902234b652b240241a7bd82d5eaadf63279
tobiasrausch <rauschtobi@gmail.com>
2016-11-14 14:40:35 +0100

    add SM tag

--- src/nRex.sh
+++ src/nRex.sh
@@ -30,13 +30,13 @@ TABIX=${BASEDIR}/htslib/tabix
 BGZIP=${BASEDIR}/htslib/bgzip
 VEP=${BASEDIR}/vep/variant_effect_predictor.pl
 VEP_DATA=${BASEDIR}/vep/cache
-JAVA=java
+JAVA=/g/software/bin/java-8
 
 # Tmp directory
 DSTR=$(date +'%a_%y%m%d_%H%M')
 export TMP=/tmp/tmp_nrex_${DSTR}
 mkdir -p ${TMP}
-JAVAOPT="-Xms4g -Xmx8g -XX:ParallelGCThreads=4 -Djava.io.tmpdir=${TMP}"
+JAVAOPT="-Xms4g -Xmx16g -XX:ParallelGCThreads=4 -Djava.io.tmpdir=${TMP}"
 PICARDOPT="MAX_RECORDS_IN_RAM=5000000 TMP_DIR=${TMP} VALIDATION_STRINGENCY=SILENT"
 
 # Align
@@ -51,7 +51,7 @@ do
     BAMID=`echo ${OUTP} | sed "s/$/.align${i}/"`
 
     # BWA
-    ${BWA} mem -t ${THREADS} ${GENOME} ${SAMPLEARR[$i]} ${SAMPLEARR[$j]} | ${SAM} view -bT ${GENOME} - > ${OUTP}/${BAMID}.bam
+    ${BWA} mem -t ${THREADS} -R "@RG\tID:${OUTP}\tSM:${OUTP}" ${GENOME} ${SAMPLEARR[$i]} ${SAMPLEARR[$j]} | ${SAM} view -bT ${GENOME} - > ${OUTP}/${BAMID}.bam
 
     # Sort & Index
     ${SAM} sort -o ${OUTP}/${BAMID}.srt.bam ${OUTP}/${BAMID}.bam && rm ${OUTP}/${BAMID}.bam && ${SAM} index ${OUTP}/${BAMID}.srt.bam
