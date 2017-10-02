d38e75e7f970ec235679a50549237b893d15f682
tobiasrausch <rauschtobi@gmail.com>
2017-10-02 16:06:38 +0200

    renamed chrs

--- src/vep.sh
+++ src/vep.sh
@@ -25,17 +25,25 @@ export PERL5LIB=${BASEDIR}/BioDbBuild/lib/perl5/x86_64-linux-thread-multi:${PERL
 # CMD parameters
 VCF=${1}.input.vcf
 ID=`echo ${VCF} | sed 's/.vcf.gz$//'`
-THREADS=8
+THREADS=4
 
 # Programs
 VEP=${BASEDIR}/vep/vep
 VEP_DATA=${BASEDIR}/vepcache
 
+# Rename chrs
+rm -f rn.chrs
+for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y
+do
+    echo chr${CHR} ${CHR} >> rn.chrs
+done
+
 # Decompose multi-allelics and normalize InDels
-bcftools norm --threads ${THREADS} -f ${VEP_DATA}/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa -m -both ${1} > ${VCF}
+bcftools annotate --rename-chrs rn.chrs ${1} | bcftools norm --threads ${THREADS} -f ${VEP_DATA}/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa -m -both - > ${VCF}
+rm rn.chrs
 
 # VEP
-${VEP} --fork ${THREADS} --species homo_sapiens --assembly GRCh37 --offline --everything --stats_file ${ID}.vep.html --format vcf --cache --dir ${VEP_DATA} --fasta ${VEP_DATA}/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa --input_file ${VCF} --vcf --output_file ${ID}.vep.vcf --no_escape --custom ${VEP_DATA}/gnomad.genomes.r2.0.1.sites.noVEP.vcf.gz,gnomADg,vcf,exact,0,AF_AFR,AF_AMR,AF_ASJ,AF_EAS,AF_FIN,AF_NFE,AF_OTH --plugin Blosum62 --plugin CSN --plugin Downstream --plugin GO --plugin LoFtool,${VEP_DATA}/Plugins/LoFtool_scores.txt --plugin TSSDistance --plugin MaxEntScan,${VEP_DATA}/Plugins/maxentscan/ --plugin MPC,${VEP_DATA}/Plugins/fordist_constraint_official_mpc_values_v2.txt.gz --plugin ExACpLI,${VEP_DATA}/Plugins/ExACpLI_values.txt
+${VEP} --fork ${THREADS} --species homo_sapiens --assembly GRCh37 --offline --everything --no_stats --format vcf --cache --dir ${VEP_DATA} --fasta ${VEP_DATA}/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa --input_file ${VCF} --vcf --output_file ${ID}.vep.vcf --no_escape --custom ${VEP_DATA}/gnomad.genomes.r2.0.1.sites.noVEP.vcf.gz,gnomADg,vcf,exact,0,AF_AFR,AF_AMR,AF_ASJ,AF_EAS,AF_FIN,AF_NFE,AF_OTH --plugin Blosum62 --plugin CSN --plugin Downstream --plugin GO --plugin LoFtool,${VEP_DATA}/Plugins/LoFtool_scores.txt --plugin TSSDistance --plugin MaxEntScan,${VEP_DATA}/Plugins/maxentscan/ --plugin MPC,${VEP_DATA}/Plugins/fordist_constraint_official_mpc_values_v2.txt.gz --plugin ExACpLI,${VEP_DATA}/Plugins/ExACpLI_values.txt
 bgzip ${ID}.vep.vcf
 tabix ${ID}.vep.vcf.gz
 rm ${VCF}
