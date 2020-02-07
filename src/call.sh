484807d07def7985523ada422a1cc0af4698babf
tobiasrausch <rauschtobi@gmail.com>
2020-02-07 16:54:35 +0100

    strelka

--- src/call.sh
+++ src/call.sh
@@ -10,9 +10,11 @@ fi
 
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
+FBY=1 # Freebayes or Strelka
 
 # Add all required binaries
 export PATH=/g/funcgen/bin:${PATH}
+STRELKAROOT=/g/solexa/bin/software/strelka-2.9.2.centos6_x86_64/
 
 # CMD parameters
 ATYPE=${1}
@@ -27,9 +29,17 @@ then
     export TMP=${SCRATCHDIR}
 fi
 
-# Freebayes
-freebayes --no-partial-observations --min-repeat-entropy 1 --report-genotype-likelihood-max --min-alternate-fraction 0.15 --fasta-reference ${GENOME} --genotype-qualities $@ -v ${OUTP}.vcf
-#freebayes --no-partial-observations --min-repeat-entropy 1 --report-genotype-likelihood-max --min-alternate-fraction 0.01 --fasta-reference ${GENOME} --genotype-qualities $@ -v ${OUTP}.vcf
+# Freebayes or Strelka
+if [ ${FBY} -eq 1 ]
+then
+    freebayes --no-partial-observations --min-repeat-entropy 1 --report-genotype-likelihood-max --min-alternate-fraction 0.15 --fasta-reference ${GENOME} --genotype-qualities $@ -v ${OUTP}.vcf
+    #freebayes --no-partial-observations --min-repeat-entropy 1 --report-genotype-likelihood-max --min-alternate-fraction 0.01 --fasta-reference ${GENOME} --genotype-qualities $@ -v ${OUTP}.vcf
+else
+    ${STRELKAROOT}/bin/configureStrelkaGermlineWorkflow.py --bam `echo ${@} | sed 's/[ \t][ \t]*/ --bam /g'` --referenceFasta ${GENOME} --runDir ${OUTP}_strelkaWES
+    ${OUTP}_strelkaWES/runWorkflow.py -m local -j ${THREADS}
+    bcftools view -f PASS ${OUTP}_strelkaWES/results/variants/variants.vcf.gz > ${OUTP}.vcf
+    rm -rf ${OUTP}_strelkaWES
+fi
 cat ${OUTP}.vcf | grep "^#" > ${OUTP}.vcf.tmp
 cat ${OUTP}.vcf| grep -v "^#" | sort -k1,1V -k2,2n >> ${OUTP}.vcf.tmp
 mv ${OUTP}.vcf.tmp ${OUTP}.vcf
@@ -41,17 +51,23 @@ bcftools norm -O z -o ${OUTP}.norm.vcf.gz -f ${GENOME} -m -both ${OUTP}.vcf.gz
 tabix ${OUTP}.norm.vcf.gz
 rm ${OUTP}.vcf.gz ${OUTP}.vcf.gz.tbi
 
-# Fixed threshold filtering
-if [[ ${ATYPE} = *"haloplex"* ]]; then
-    # Stringent parameters
-    bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=2 || SAR<=2' ${OUTP}.norm.vcf.gz
-    # Relaxed parameters
-    #bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=1 || SAR<=1' ${OUTP}.norm.vcf.gz
-else
-    # Stringent parameters
-    bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=2 || SAR<=2 || RPR<=2 || RPL<=2' ${OUTP}.norm.vcf.gz
-    # Relaxed parameters
-    #bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=1 || SAR<=1 || RPR<=0 || RPL<=0' ${OUTP}.norm.vcf.gz
+# Filtering
+if [ ${FBY} -eq 1 ]
+then
+    # Fixed threshold filtering
+    if [[ ${ATYPE} = *"haloplex"* ]]; then
+	# Stringent parameters
+	bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=2 || SAR<=2' ${OUTP}.norm.vcf.gz
+	# Relaxed parameters
+	#bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=1 || SAR<=1' ${OUTP}.norm.vcf.gz
+    else
+	# Stringent parameters
+	bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=2 || SAR<=2 || RPR<=2 || RPL<=2' ${OUTP}.norm.vcf.gz
+	# Relaxed parameters
+	#bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20 || %QUAL/INFO/AO<=2 || SAF<=1 || SAR<=1 || RPR<=0 || RPL<=0' ${OUTP}.norm.vcf.gz
+    fi
+else 
+    bcftools filter -O z -o ${OUTP}.norm.filtered.vcf.gz -e '%QUAL<=20' ${OUTP}.norm.vcf.gz
 fi
 tabix ${OUTP}.norm.filtered.vcf.gz
 rm ${OUTP}.norm.vcf.gz ${OUTP}.norm.vcf.gz.tbi
