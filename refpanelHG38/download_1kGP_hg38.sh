3d8c31bc1f14aa2da4dad16665fc6f8e02418d21
tobiasrausch <rauschtobi@gmail.com>
2020-10-14 11:50:43 +0200

    fix hg38

--- refpanelHG38/download_1kGP_hg38.sh
+++ refpanelHG38/download_1kGP_hg38.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-#wget -O- ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz | gzip -d > GCA_000001405.15_GRCh38_no_alt_analysis_set.fna  
-#samtools faidx GCA_000001405.15_GRCh38_no_alt_analysis_set.fna  
-#wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chr{{1..22},X,Y}_GRCh38.genotypes.20170504.vcf.gz{,.tbi}
- 
-#for chr in {1..22} X Y; do  
-#    (bcftools view --no-version -h ALL.chr${chr}_GRCh38.genotypes.20170504.vcf.gz | grep -v "^##contig=<ID=[GNh]" | sed 's/^##contig=<ID=MT/##contig=<ID=chrM/;s/^##contig=<ID=\([0-9XY]\)/##contig=<ID=chr\1/'; bcftools view --no-version -H -c 2 ALL.chr${chr}_GRCh38.genotypes.20170504.vcf.gz | sed 's/^/chr/') | bcftools norm --no-version -Ou -m -any | bcftools norm --no-version -Ob -o ALL.chr${chr}_GRCh38.genotypes.20170504.bcf -d none -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna && bcftools index -f ALL.chr${chr}_GRCh38.genotypes.20170504.bcf  
-#done
+#wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20190312_biallelic_SNV_and_INDEL/*
 
-# Rename
-for F in *.bcf
+for CHR in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX
 do
-    ID=`echo ${F} | sed 's/ALL.//' | sed 's/_.*$/.bcf/'`
-    echo ${ID}
-    #mv ${F} ${ID}
-    #mv ${F}.csi ${ID}.csi
+    bcftools view -O b -o ${CHR}.bcf --no-version ALL.${CHR}.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz
+    bcftools index ${CHR}.bcf
+    rm ALL.${CHR}.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz
 done
-
-# Concatenate without chrY
-bcftools concat chr1.bcf chr2.bcf chr3.bcf chr4.bcf chr5.bcf chr6.bcf chr7.bcf chr8.bcf chr9.bcf chr10.bcf chr11.bcf chr12.bcf chr13.bcf chr14.bcf chr15.bcf chr16.bcf chr17.bcf chr18.bcf chr19.bcf chr20.bcf chr21.bcf chr22.bcf chrX.bcf | cut -f 1-8 | bcftools view -O b -o sites.bcf -
-bcftools index sites.bcf
