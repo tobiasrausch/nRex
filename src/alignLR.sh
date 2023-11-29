686ee8767a2d5b9421919b94e2517fae717778db
Tobias Rausch <rauschtobi@gmail.com>
2023-11-29 13:08:01 +0100

    minimap2

---
+++ src/alignLR.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]
+then
+    echo ""
+    echo "Usage: $0 <hg38.wgs|hg38.wes|hg38.haloplex> <genome.fa> <output prefix> <sample1.fq.gz>"
+    echo ""
+    exit -1
+fi
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+export PATH=${BASEDIR}/../mamba/bin:${PATH}
+
+# CMD parameters
+ATYPE=${1}
+GENOME=${2}
+OUTP=${3}
+FQ=${4}
+THREADS=8
+
+# BWA alignment
+minimap2 -t ${THREADS} -a -x map-ont -L ${GENOME} ${FQ} | samtools sort -o ${OUTP}.bam
+samtools index ${OUTP}.bam
+
+# Run stats using unfiltered BAM
+samtools idxstats -@ ${THREADS} ${OUTP}.bam > ${OUTP}.idxstats
+samtools flagstat -@ ${THREADS} ${OUTP}.bam > ${OUTP}.flagstat
+
+# Run alfred for BAM statistics
+alfred qc -b ${BASEDIR}/../genome/${ATYPE}.bed.gz -r ${GENOME} -j ${OUTP}.alfred.json.gz -o ${OUTP}.alfred.tsv.gz ${OUTP}.bam
---
+++ src/nRexLR.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "**********************************************************************"
+    echo "nRex: Single-nucleotide variant calling."
+    echo "This program comes with ABSOLUTELY NO WARRANTY."
+    echo ""
+    echo "nRex (Version: 0.1.8)"
+    echo "Contact: Tobias Rausch (rausch@embl.de)"
+    echo "**********************************************************************"
+    echo ""
+    echo "Usage: $0 <output prefix> <sample.fq.gz>"
+    echo ""
+    exit -1
+fi
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+
+# CMD parameters
+VERSION=38
+ATYPE=hg${VERSION}.wgs    # [hg38.wgs|hg38.wes|hg38.haloplex]
+GENOME=${BASEDIR}/../genomeLR/hg${VERSION}.fa
+MAP=${BASEDIR}/../genome/Homo_sapiens.GRCh${VERSION}.dna.primary_assembly.fa.r101.s501.blacklist.gz
+OUTP=${1}
+FQ=${2}
+
+if [ ! -f ${OUTP}.bam ]
+then
+    ${BASEDIR}/alignLR.sh ${ATYPE} ${GENOME} ${OUTP} ${FQ}
+fi
+
+# Calculate coverage
+if [ ! -f ${OUTP}.cov.gz ]
+then
+    ${BASEDIR}/coverage.sh ${ATYPE} ${GENOME} ${MAP} ${OUTP} ${OUTP}.bam
+fi
+
+# Structural variants [can be jointly run on multiple BAM files]
+if [ ! -f ${OUTP}.delly.bcf ]
+then
+    ${BASEDIR}/delly.sh ${ATYPE} ${GENOME} ${OUTP} ${OUTP}.bam
+fi
