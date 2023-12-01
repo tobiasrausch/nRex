d35efa890d0ef89ade9ef84846aa2cd14b1e45e0
Tobias Rausch <rausch@embl.de>
2023-12-01 10:40:25 +0100

    fix paths

--- genome/prepare_genome.sh
+++ genome/prepare_genome.sh
@@ -3,7 +3,6 @@
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 export PATH=${BASEDIR}/../conda/bin:${PATH}
-source activate align
 
 # GRCh38
 wget ftp://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/references/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta.gz
@@ -15,7 +14,7 @@ rm GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta.gz
 bwa index hg38.fa
 
 # Mappability and exclude maps
-wget https://gear.embl.de/data/delly/Homo_sapiens.GRCh38.dna.primary_assembly.fa.r101.s501.blacklist.gz
+wget https://gear-genomics.embl.de/data/delly/Homo_sapiens.GRCh38.dna.primary_assembly.fa.r101.s501.blacklist.gz
 samtools faidx Homo_sapiens.GRCh38.dna.primary_assembly.fa.r101.s501.blacklist.gz
 wget https://raw.githubusercontent.com/dellytools/delly/main/excludeTemplates/human.hg38.excl.tsv
 
@@ -24,8 +23,8 @@ cat hg38.fa.fai  | awk '{print $1"\t1\t"$2;}' | grep -P "^chr[0-9XYM]*\t" > hg38
 bgzip hg38.wgs.bed 
 
 # Exome bed
-wget ftp://ftp.ensembl.org/pub/current_gff3/homo_sapiens/Homo_sapiens.GRCh38.[0-9][0-9][0-9].gff3.gz
-wget ftp://ftp.ensembl.org/pub/current_gtf/homo_sapiens/Homo_sapiens.GRCh38.[0-9][0-9][0-9].gtf.gz
+wget http://ftp.ensembl.org/pub/current_gff3/homo_sapiens/Homo_sapiens.GRCh38.110.gff3.gz
+wget http://ftp.ensembl.org/pub/current_gtf/homo_sapiens/Homo_sapiens.GRCh38.110.gtf.gz
 zcat Homo_sapiens.GRCh38.*.gtf.gz | grep "ensembl_havana" | grep "exon" | grep "protein_coding" | grep "gene_name" | cut -f 1,4,5,9 | sed 's/\t[^\t]*gene_name "/\t/' | sed 's/".*$//' | uniq | sed 's/^/chr/' | sort -k1,1V -k2,2n | uniq > hg38.wes.bed.tmp
 bedtools merge -i hg38.wes.bed.tmp -c 4 -o mode > hg38.wes.bed
 rm hg38.wes.bed.tmp
