a6961a42e4762dafd1844c75717f77c37b610c20
Tobias Rausch <rausch@embl.de>
2023-12-01 10:41:05 +0100

    fix paths

--- genome/prepare_genome.sh
+++ genome/prepare_genome.sh
@@ -2,7 +2,7 @@
 
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
-export PATH=${BASEDIR}/../conda/bin:${PATH}
+export PATH=${BASEDIR}/../mamba/bin:${PATH}
 
 # GRCh38
 wget ftp://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/references/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta.gz
