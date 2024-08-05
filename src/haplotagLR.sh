a5b4bdf697108cb921ee029a78494cc439b7d895
Tobias Rausch <rauschtobi@gmail.com>
2024-08-05 15:44:32 +0200

    use longshot

--- src/haplotagLR.sh
+++ src/haplotagLR.sh
@@ -18,23 +18,15 @@ OUTP=${1}
 GENOME=${2}
 BAM=${3}
 
-# Annotate common SNPs
-bcftools mpileup --ignore-RG -a AD -B -Q 0 -q 1 -I -f ${GENOME} ${BAM} | bcftools call -P 0.01 -mv -V indels - | bcftools norm -f ${GENOME} -m -both -  | bcftools filter -O b -o ${OUTP}.tmp.bcf -i 'DP>=5 && AD[:1]>=2' -
-bcftools index ${OUTP}.tmp.bcf
-bcftools isec -O b -o ${OUTP}.common.bcf -n =2 -w 1 ${OUTP}.tmp.bcf ${BASEDIR}/../genomeLR/common_sites.vcf.gz
-bcftools index ${OUTP}.common.bcf
-rm ${OUTP}.tmp.bcf ${OUTP}.tmp.bcf.csi
-
-# Fetch input variants
-bcftools annotate ${OUTP}.common.bcf -x INFO,^FORMAT/GT | bgzip > ${OUTP}.called.vcf.gz
-tabix ${OUTP}.called.vcf.gz
-rm ${OUTP}.common.bcf ${OUTP}.common.bcf.csi
+# Genotype known variants
+longshot --no_haps --min_cov 3 --potential_variants ${BASEDIR}/../genomeLR/common_sites.vcf.gz --min_alt_count 2 --min_alt_frac 0.01 -s ${OUTP} --bam ${BAM} --ref ${GENOME} --out ${OUTP}.longshot.vcf
+bgzip ${OUTP}.longshot.vcf
+tabix ${OUTP}.longshot.vcf.gz
 
 # Generate phased blocks
-whatshap phase --ignore-read-groups --reference ${GENOME} ${OUTP}.called.vcf.gz ${BAM} -o ${OUTP}.whatshap.vcf
+whatshap phase --ignore-read-groups --reference ${GENOME} ${OUTP}.longshot.vcf.gz ${BAM} -o ${OUTP}.whatshap.vcf
 bgzip ${OUTP}.whatshap.vcf
 tabix ${OUTP}.whatshap.vcf.gz
-rm ${OUTP}.called.vcf.gz ${OUTP}.called.vcf.gz.tbi
 
 # Haplotag reads
 whatshap haplotag -o ${OUTP}.haplotagged.bam --reference ${GENOME} --ignore-read-groups --tag-supplementary --skip-missing-contigs ${OUTP}.whatshap.vcf.gz ${BAM}
